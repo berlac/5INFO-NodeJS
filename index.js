36ec6c8a89b1793c5b32664c34e3ea220f30deb0
Bernardo Lacerda <lacerdabernardo91@gmail.com>
2020-10-28 10:37:39 -0300

    Updated index.js

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const path = require('path');
 
 const app = express();
+
+app.set('view engine','ejs');
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.use(cookieParser());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function(req, res){
     res.send('Olá Bernardo Lacerda!!');
